770ac1f2dbc50f6cbb4eb8c0182a5c5a3d535fd8
Marie Sumalee <81355905+MarieSumalee@users.noreply.github.com>
2025-03-10 13:06:42 +1100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ function showDivs(n) {
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display ="none";  
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" w3-white", "");
